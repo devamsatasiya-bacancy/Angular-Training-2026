f56416ffb7f14cbf4e7ab20689f8cc4e238aadb6
devamsatasiya-bacancy <devam.satasiya@bacancy.us>
2026-04-02 18:56:37 +0530

    Delete Sessions/17-guards-in-angular directory

--- Sessions/17-guards-in-angular/src/app/core/resolvers/posts-resolver.ts
+++
@@ -1,8 +0,0 @@
-import { inject } from '@angular/core';
-import { ResolveFn } from '@angular/router';
-import { Post } from '../models/post.model';
-import { Posts } from '../services/posts';
-
-export const postsResolver: ResolveFn<Post[]> = () => {
-  return inject(Posts).getPosts();
-};
