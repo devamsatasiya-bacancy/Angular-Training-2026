9bb7f5d1a1269ae6dd7523f02f397298b7f774a8
devamsatasiya-work <devam.satasiya@bacancy.us>
2026-04-06 11:37:55 +0530

    currently no environement file present

--- Sessions/22-making-http-requests-part-2/src/environments/environment.development.ts
+++
@@ -1,4 +0,0 @@
-export const devEnvironment = {
-    production: false,
-    firebaseUrl: 'https://social-media-app-test-ng-default-rtdb.asia-southeast1.firebasedatabase.app/books-session22.json'
-};
--- Sessions/22-making-http-requests-part-2/src/environments/environment.ts
+++
@@ -1,4 +0,0 @@
-export const prodEnvironment = {
-    production: true,
-    firebaseUrl: 'https://social-media-app-test-ng-default-rtdb.asia-southeast1.firebasedatabase.app/books.json'
-};
